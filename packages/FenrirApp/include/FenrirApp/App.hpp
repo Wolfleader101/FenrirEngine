fc8930332ce6e49ca296b17c99556eda9367f8c4
Wolfleader <james.morgan@iinet.net.au>
2023-11-22 11:39:35 +0800

    scheduler approach similar to bevy

--- packages/FenrirApp/include/FenrirApp/App.hpp
+++ packages/FenrirApp/include/FenrirApp/App.hpp
@@ -1,7 +1,47 @@
-#include <string>
+#include <functional>
+#include <initializer_list>
+#include <map>
 
 namespace Fenrir
 {
+
+    // bevy wraps systems in a class with extra methods such as "before", "after", "run_if", for now i wont add these
+    // but i might in the future
+    using SystemFunc = std::function<void()>;
+
+    // systems should be able to be added to the app
+    // a system should just be a function that is called at a certain point in the app's lifecycle
+    // when adding a system it should also have a priority, and what functions it should hook into
+
+    // schedule should just be an enum
+    enum class SchedulePriority
+    {
+        PreInit,
+        Init,
+        PostInit,
+        PreUpdate,
+        Tick,
+        Update,
+        PostUpdate,
+        // CreateScene,
+        Last,
+    };
+
+    class Scheduler
+    {
+      public:
+        Scheduler();
+
+        Scheduler& AddSystems(SchedulePriority priority, std::initializer_list<SystemFunc> systems);
+
+        Scheduler& AddSystem(SchedulePriority priority, SystemFunc system);
+
+        void Run();
+
+      private:
+        std::map<SchedulePriority, std::vector<SystemFunc>> m_systems;
+    };
+
     class App
     {
       public:
@@ -9,11 +49,14 @@ namespace Fenrir
 
         App& Init();
 
-        App& AddSystem(std::string test);
+        App& AddSystems(SchedulePriority priority, std::initializer_list<SystemFunc> systems);
+
+        App& AddSystem(SchedulePriority priority, SystemFunc system);
 
         void Run();
 
       private:
-        int x = 0;
+        Scheduler m_scheduler;
     };
+
 } // namespace Fenrir
--- packages/FenrirApp/src/App.cpp
+++ packages/FenrirApp/src/App.cpp
@@ -1,36 +1,65 @@
 #include "FenrirApp/App.hpp"
 
-#include <iostream>
-
 namespace Fenrir
 {
     App::App()
     {
-        std::cout << "hello from app" << std::endl;
-        std::cout << "Constructor X = " << x << std::endl;
-        x++;
     }
 
     App& App::Init()
     {
-        std::cout << "Init X = " << x << std::endl;
-        x++;
         return *this;
     }
 
-    App& App::AddSystem(std::string test)
+    App& App::AddSystems(SchedulePriority priority, std::initializer_list<SystemFunc> systems)
     {
-        std::cout << "hello from system " << test << std::endl;
-        std::cout << "AddSystem X = " << x << std::endl;
-        x++;
+        m_scheduler.AddSystems(priority, systems);
+        return *this;
+    }
+
+    App& App::AddSystem(SchedulePriority priority, SystemFunc system)
+    {
+        m_scheduler.AddSystem(priority, system);
         return *this;
     }
 
     void App::Run()
     {
-        std::cout << "hello from run" << std::endl;
-        std::cout << "Run X = " << x << std::endl;
-        x++;
+        m_scheduler.Run();
+    }
+
+    Scheduler::Scheduler()
+    {
+    }
+
+    Scheduler& Scheduler::AddSystems(SchedulePriority priority, std::initializer_list<SystemFunc> systems)
+    {
+        // for each system, add it to the map
+        for (auto system : systems)
+        {
+            m_systems[priority].push_back(system);
+        }
+
+        return *this;
+    }
+
+    Scheduler& Scheduler::AddSystem(SchedulePriority priority, SystemFunc system)
+    {
+        // add the system to the map
+        m_systems[priority].push_back(system);
+        return *this;
+    }
+
+    void Scheduler::Run()
+    {
+        // for each priority, run each system in order of their insertion
+        for (auto& [priority, systems] : m_systems)
+        {
+            for (auto& system : systems)
+            {
+                system(); // TODO this might need to be a wrapper, so it can take in Scene??
+            }
+        }
     }
 
 } // namespace Fenrir
