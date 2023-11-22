b6750fea34993303059f5ad69dd526b442c408c7
Wolfleader <james.morgan@iinet.net.au>
2023-11-22 10:44:51 +0800

    initial files and example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FenrirEngine
+
+Modular Engine designed from the ground up to allow users to use only the modules they want to use.
---
+++ examples/DemoApp/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "FenrirApp/App.hpp"
+
+int main()
+{
+    std::cout << "hello world" << std::endl;
+
+    Fenrir::App app;
+}
