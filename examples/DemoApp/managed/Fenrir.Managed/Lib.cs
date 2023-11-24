13b35eaec26baa834f706276d62356a6681be8fa
Wolfleader <james.morgan@iinet.net.au>
2023-11-24 08:50:37 +0800

    Fenrir.Managed working

--- examples/DemoApp/managed/Fenrir.Managed/Lib.cs
+++ examples/DemoApp/managed/Fenrir.Managed/Lib.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Fenrir.Managed
 {    public static class Lib
     {
         private static int s_CallCount = 1;
 
-        public static int Hello()
+        [UnmanagedCallersOnlyAttribute]
+        public static void Hello()
         {
+            Console.WriteLine($"Hello, world! from {nameof(Lib)} [count: {s_CallCount}]");
             s_CallCount++;
-            return s_CallCount;
         }
     }
 }
